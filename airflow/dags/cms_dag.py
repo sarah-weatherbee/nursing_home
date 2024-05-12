af5979ccc29e57427b3fd22fd518668579c7ffb1
Sarah Weatherbee <sarahmweatherbee@gmail.com>
2024-05-11 22:16:14 -0400

    streamlined cms_dag functions

--- airflow/dags/cms_dag.py
+++ airflow/dags/cms_dag.py
@@ -12,11 +12,12 @@ import time
 
 url = "https://data.cms.gov/data.json"
 title = "COVID-19 Nursing Home Data"
-def get_endpoint():
 
+def get_endpoint():
     response = requests.request("GET",url)
     if response.ok:
         response = response.json()
+        print("response ok")
         dataset = response['dataset']
     for set in dataset:
         if title ==set['title']:
@@ -24,45 +25,49 @@ def get_endpoint():
                 if 'format' in distro.keys() and 'description' in distro.keys():
                     if distro['format'] == "API" and distro['description'] == "latest":
                         latest_distro = distro['accessURL']
+                        print(f"The latest data for {title} can be found at {latest_distro} or {set['identifier']}")
+
     return latest_distro
 
 def extract_raw_cms_data(latest_distro):
-  latest_data = []
-  stats_endpoint = latest_distro + "/stats"
-  stats_response = requests.request("GET", stats_endpoint)
-  stats_response = stats_response.json()
-  total_rows = stats_response['total_rows']
-#print(f"total rows: {total_rows}")
+    latest_distro = get_endpoint()
+    stats_endpoint = latest_distro + "/stats"
+    print(f"stats endpoint: {stats_endpoint}")
 
-  date_today = date.today()
-  end_wk_end_date = date_today - relativedelta(weeks=1, weekday=SU)
-  start_wk_end_date = end_wk_end_date - relativedelta(weeks=2, weekday=SU)
-  week = timedelta(days=7)
-  offset = 0
-  size = 5000
+    latest_raw_data = []
+    stats_response = requests.request("GET", stats_endpoint).json()
+    total_rows = stats_response['total_rows']
+    print(f"total rows: {total_rows}")
+    date_today = date.today()
+    end_wk_end_date = date_today - relativedelta(weeks=1, weekday=SU)
+    start_wk_end_date = end_wk_end_date - relativedelta(weeks=2, weekday=SU)
+    week = timedelta(days=7)
+    offset = 0
+    size = 5000
 
-  while start_wk_end_date <= end_wk_end_date:
-      for offset in range(0,total_rows,size):
-          offset_url = f"{latest_distro}?filter[week_ending]={start_wk_end_date}&offset={offset}&size={size}"
-          offset_response = requests.request("GET", offset_url)
-          data = offset_response.json()
-          print(f"Made request for {size} results at offset {offset}")
-          if len(data) == 0:
-                  break
-          latest_data.extend(data)
-          offset = offset + size
-          time.sleep(3)
-          print("---")
-          current_url = f"{latest_distro}?filter[week_ending]={start_wk_end_date}&offset={offset}&size={size}"
-          print("Requesting",current_url)
+    while start_wk_end_date <= end_wk_end_date:
+        for offset in range(0,total_rows,size):
+            offset_url = f"{latest_distro}?filter[week_ending]={start_wk_end_date}&offset={offset}&size={size}"
+            offset_response = requests.request("GET", offset_url)
+            data = offset_response.json()
+            print(f"Made request for {size} results at offset {offset}")
+            if len(data) == 0:
+                    break
+            latest_raw_data.extend(data)
+            offset = offset + size
+            time.sleep(3)
+            print("---")
+            current_url = f"{latest_distro}?filter[week_ending]={start_wk_end_date}&offset={offset}&size={size}"
+            print("Requesting",current_url)
 
-          start_wk_end_date = start_wk_end_date + week
+        start_wk_end_date = start_wk_end_date + week
 
-          df_latest_data = pd.DataFrame(latest_data)
-          json_latest_data = json.dumps(latest_data)
-          # save as parquet file
-          # pq_latest_data = df_latest_data.to_parquet("data_pre_proc/nh_pre_proc_raw.parquet", engine='auto', compression='snappy', index=None, partition_cols=None)
-  return df_latest_data, json_latest_data
+    df_latest_raw_data = pd.DataFrame(latest_raw_data)
+    ## save as json
+    # json_latest_data = json.dumps(latest_data)
+    # save as parquet file
+    pq_latest_raw_data = df_latest_raw_data.to_parquet("data_pre_proc/nh_pre_proc_raw.parquet", engine='auto', compression='snappy', index=None, partition_cols=None)
+    return df_latest_raw_data, pq_latest_raw_data
 
 
 
--- extract_cms_data.py
+++ extract_cms_data.py
@@ -1,5 +1,4 @@
 # To be run weekly, on Tues. Pulls data from CMS api.
-
 import pandas as pd
 import requests
 import json
@@ -9,12 +8,10 @@ from dateutil.relativedelta import relativedelta, MO, SU
 import time
 #import boto3
 
-def extract_cms_data():
+url = "https://data.cms.gov/data.json"
+title = "COVID-19 Nursing Home Data"
 
-    url = "https://data.cms.gov/data.json"
-    title = "COVID-19 Nursing Home Data"
-
-
+def get_endpoint():
     response = requests.request("GET",url)
     if response.ok:
         response = response.json()
@@ -25,44 +22,34 @@ def extract_cms_data():
                 if 'format' in distro.keys() and 'description' in distro.keys():
                     if distro['format'] == "API" and distro['description'] == "latest":
                         latest_distro = distro['accessURL']
-                        latest_distro_ep = distro
-                        print(f"distro{distro}")
-                        print(f"latest distro ep {latest_distro_ep}")
                         print(f"The latest data for {title} can be found at {latest_distro} or {set['identifier']}")
 
+def extract_raw_cms_data():
+    latest_distro = get_endpoint()
     stats_endpoint = latest_distro + "/stats"
     print(f"stats endpoint: {stats_endpoint}")
 
-    latest_data = []
-    stats_response = requests.request("GET", stats_endpoint)
-    stats_response = stats_response.json()
+    latest_raw_data = []
+    stats_response = requests.request("GET", stats_endpoint).json()
     total_rows = stats_response['total_rows']
-    #print(f"total rows: {total_rows}")
-
-
+    print(f"total rows: {total_rows}")
 
     date_today = date.today()
     end_wk_end_date = date_today - relativedelta(weeks=1, weekday=SU)
-
     start_wk_end_date = end_wk_end_date - relativedelta(weeks=2, weekday=SU)
-
     week = timedelta(days=7)
     offset = 0
     size = 5000
 
-
     while start_wk_end_date <= end_wk_end_date:
         for offset in range(0,total_rows,size):
             offset_url = f"{latest_distro}?filter[week_ending]={start_wk_end_date}&offset={offset}&size={size}"
-
-
             offset_response = requests.request("GET", offset_url)
             data = offset_response.json()
             print(f"Made request for {size} results at offset {offset}")
             if len(data) == 0:
                     break
-            latest_data.extend(data)
-
+            latest_raw_data.extend(data)
             offset = offset + size
 
             time.sleep(3)
@@ -72,16 +59,21 @@ def extract_cms_data():
 
         start_wk_end_date = start_wk_end_date + week
 
-    df_latest_data = pd.DataFrame(latest_data)
-    json_latest_data = json.dumps(latest_data)
+    df_latest_raw_data = pd.DataFrame(latest_raw_data)
+    ## save as json
+    # json_latest_data = json.dumps(latest_data)
     #save as parquet file
-    #pq_latest_data = df_latest_data.to_parquet("data_pre_proc/nh_pre_proc_raw.parquet", engine='auto', compression='snappy', index=None, partition_cols=None)
-    return df_latest_data,json_latest_data
+    pq_latest_raw_data = df_latest_raw_data.to_parquet("data_pre_proc/nh_pre_proc_raw.parquet", engine='auto', compression='snappy', index=None, partition_cols=None)
+    return df_latest_raw_data, pq_latest_raw_data
 
-extract_cms_data()
+def transform_cms_data():
+    print("WIP it")
+
+def main():
+  get_endpoint()
+  extract_raw_cms_data()
+
+main()
 #save as csv file
 #df_latest_data.to_csv("data_pre_proc/nh_pre_proc_raw.csv", index=False)
-
-#save concated as updated parquet file
-# df_latest_data.to_parquet('data_pre_proc/nh_all_Jan8_23_Aug6_23_concat.parquet.gzip', compression='gzip')
 
